19cb555f7524c073e68e3dec113fd6dd2ba94f42
Carlos Parra <carlos.parra@uvee.co>
2017-10-26 15:19:33 -0500

    Se agregan archivos base de plantilla

---
+++ src/CitasBundle/Controller/CitasController.php
@@ -0,0 +1,17 @@
+<?php
+
+    namespace CitasBundle\Controller;
+
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+    class CitasController extends Controller {
+
+        /**
+         * @Route(path="/", name="citas_search")
+         */
+        public function searchAction() {
+
+            
+        }
+    }
